5200f7883a8e9a821200788fa6d09dd5d520c28d
Avirat201189221 <avirat0603@gmail.com>
2024-06-18 19:19:08 +0530

    project init

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        {/* <Navbar></Navbar> */}
+    </div>
+  );
+}
+
+export default App;
